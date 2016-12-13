afcb87d63e5933980b8ea67fe0d1c6295a0d69c2
Louis Tur <louis.tur@gmail.com>
2016-12-12 19:19:28 -0500

    xcproj push

---
+++ AC3.2-Programmatic-Autolayout/AC3.2-Programmatic-Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AC3.2-Programmatic-Autolayout
+//
+//  Created by Louis Tur on 12/12/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
